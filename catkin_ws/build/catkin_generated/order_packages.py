8508464058c243198cb4a835fffea985b865a7af
Brian De La Cruz <Brian_dela_cruz@outlook.com>
2024-02-08 18:23:35 +0000

    Generated basic project structure for turtlebot2

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/briandelacruz/turtlebot2_teleop/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/briandelacruz/docker_turtlesim_tutorial/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/briandelacruz/docker_turtlesim_tutorial/catkin_ws/devel;/opt/ros/noetic' != '' else []
